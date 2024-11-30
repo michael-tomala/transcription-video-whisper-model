5f8da32b3e9e2967b8a73948d737ff05c00f3f90
michal.tomala <michal.tomala@lama-media.com>
2024-11-30 20:09:40 +0100

    refactor

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ dotenv.config();
 (async () => {
     // Pobranie ścieżki do pliku wideo z argumentów przekazanych przy uruchamianiu aplikacji
     const videoPath = process.argv[2]; // Pierwszy argument to ścieżka do pliku wideo
+
     if (!videoPath) {
         console.error('Brak ścieżki do pliku wideo! Proszę podać ścieżkę jako argument.');
         process.exit(1); // Zakończ aplikację, jeśli ścieżka nie została podana
@@ -22,29 +23,27 @@ dotenv.config();
         console.error('Brak klucza OPENAI_API_KEY w pliku .env');
         process.exit(1); // Zakończ aplikację, jeśli ścieżka nie została podana
     }
+
     const reformatMinWordsDuration = process.argv[3] || 500; // Pierwszy argument to ścieżka do pliku wideo
 
-    const sourceFileName = 'output/' + path.basename(videoPath).replace(/\.[^/.]+$/, "").toLowerCase().replace(' ', '')
-    const audioPath = './' + sourceFileName + '/audio.mp3'; // Ścieżka do pliku audio
+    const outputDir = 'output/' + path.basename(videoPath).replace(/\.[^/.]+$/, "").toLowerCase().replace(' ', '')
 
     try {
-        await fs.rmSync(sourceFileName, {recursive: true})
+        await fs.rmSync(outputDir, {recursive: true})
     } catch (err) {
     }
-    await fs.mkdirSync(sourceFileName)
+    await fs.mkdirSync(outputDir)
 
     try {
 
         console.log('Wyodrębnianie audio...');
-        await extractAudio(videoPath, audioPath);
+        await extractAudio(videoPath, outputDir);
 
         console.log('Sprawdzanie rozmiaru pliku audio...');
-        const chunks = await splitAudioFile(audioPath);
-
+        const chunks = await splitAudioFile(outputDir);
         console.log(`Dzielimy plik na ${chunks.length} kawałków...`);
 
         const transcriptions = [];
-
         for (let chunk of chunks) {
             console.log(`Wysyłanie pliku ${chunk} do Whisper API...`);
             const transcription = await transcribeAudio(chunk);
@@ -57,15 +56,17 @@ dotenv.config();
         console.log('Transkrypcja (verbose_json):');
         console.log(combinedTranscription);
 
-        // Zapis transkrypcji do pliku
-        fs.writeFileSync(sourceFileName + '/transcription.json', JSON.stringify(transcriptions, null, 2));
-        console.log(`Transkrypcja zapisana w ${sourceFileName}/transcription.json`);
+        await fs.writeFileSync(outputDir + '/transcription.json', JSON.stringify(transcriptions, null, 2));
+        console.log(`Transkrypcja zapisana w ${outputDir}/transcription.json`);
 
-        // Przetwarzamy słowa zgodnie z wymaganiami minimalnej długości
-        await generateReformatedTranscription(sourceFileName + '/transcription.json', reformatMinWordsDuration)
+        await generateReformatedTranscription(outputDir, reformatMinWordsDuration)
+        console.log(`Poprawiona transkrypcja została zapisana w ${outputDir}/transcription-reformatted.json`);
 
-        await generateSRT(sourceFileName + '/transcription-reformatted.json', 'words');
-        await generateSRT(sourceFileName + '/transcription-reformatted.json', 'segments');
+        await generateSRT(outputDir, 'words');
+        console.log(`Transkrypcja typu "words" została zapisana.`);
+
+        await generateSRT(outputDir, 'segments');
+        console.log(`Transkrypcja typu "segments" została zapisana.`);
 
     } catch (error) {
         console.error('Wystąpił błąd:', error);
